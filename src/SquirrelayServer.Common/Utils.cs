68c0937f76d853cb7cb64ee891b594b341fc933a
wraikny <writtenknown@gmail.com>
2021-04-20 00:37:17 +0900

    add config

---
+++ src/SquirrelayServer.Common/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquirrelayServer.Common
+{
+    public class Utils
+    {
+        public static T DeserializeJson<T>(string json, DataContractJsonSerializerSettings settings = null)
+        {
+            using var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            var serializer = new DataContractJsonSerializer(typeof(T), settings);
+            return (T)serializer.ReadObject(ms);
+        }
+
+        public static async ValueTask<T> DeserializeJsonFromFileAsync<T>(string path, DataContractJsonSerializerSettings settings = null)
+        {
+            var content = await File.ReadAllTextAsync(path);
+            return DeserializeJson<T>(content, settings);
+        }
+    }
+}
---
+++ src/SquirrelayServer.Server/Config.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace SquirrelayServer.Server
+{
+    [DataContract]
+    public sealed class GameConfig
+    {
+    }
+
+    [DataContract]
+    public sealed class Config
+    {
+        [DataMember(Name = "port")]
+        public int Port { get; private set; }
+
+        [DataMember(Name = "mexClientCount")]
+        public int MaxClientCount { get; private set; }
+
+        [DataMember(Name = "games")]
+        private readonly Dictionary<string, GameConfig> _games;
+
+        [IgnoreDataMember]
+        public IReadOnlyDictionary<string, GameConfig> Games => _games;
+
+
+        internal static ValueTask<Config> LoadAsync(string path)
+        {
+            var settings = new DataContractJsonSerializerSettings
+            {
+                UseSimpleDictionaryFormat = true
+            };
+            return Common.Utils.DeserializeJsonFromFileAsync<Config>(path, settings);
+        }
+    }
+}
