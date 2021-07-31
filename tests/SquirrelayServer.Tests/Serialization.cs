ff98ee96aaee98c56b725acd54f4b3f849d950fc
wraikny <writtenknown@gmail.com>
2021-07-31 23:29:00 +0900

    fix format

--- tests/SquirrelayServer.Tests/Serialization.cs
+++ tests/SquirrelayServer.Tests/Serialization.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 using MessagePack;
 
