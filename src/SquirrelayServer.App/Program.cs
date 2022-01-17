29167ca5de118c366783d3d6a313f616a2233c96
wraikny <writtenknown@gmail.com>
2022-01-17 16:33:08 +0900

    fix log file path

--- src/SquirrelayServer.App/Program.cs
+++ src/SquirrelayServer.App/Program.cs
@@ -24,9 +24,13 @@ namespace SquirrelayServer.App
             var output = $"[{current}] {msg}";
 
             _stream.WriteLine(output);
-            _stream.Flush();
 
             Console.WriteLine(output);
+        }
+
+        public void Flush()
+        {
+            _stream.Flush();
         }
     }
 
@@ -35,20 +39,31 @@ namespace SquirrelayServer.App
         private const string DefaultConfigPath = @"config/config.json";
         public static async Task Main(string[] args)
         {
-            using var fileStream = File.OpenWrite("log.txt");
+            const string directory = "squireelayserver-log";
+            Directory.CreateDirectory(directory);
+            var date = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            using var fileStream = File.OpenWrite($"{directory}/log_{date}.txt");
             using var streamWriter = new StreamWriter(fileStream);
+            var logger = new Logger(streamWriter);
+            NetDebug.Logger = logger;
 
-            // Set Logger
-            NetDebug.Logger = new Logger(streamWriter);
+            Console.CancelKeyPress += (_, _) => logger.Flush();
+            
+            try
+            {
+                // Load config
+                var path = args.Length > 0 && File.Exists(args[0]) ? args[0] : DefaultConfigPath;
+                var config = await Config.LoadFromFileAsync(path);
+                NetDebug.Logger?.WriteNet(NetLogLevel.Info, $"Config is loaded from '{path}'.");
 
-            // Load config
-            var path = args.Length > 0 && File.Exists(args[0]) ? args[0] : DefaultConfigPath;
-            var config = await Config.LoadFromFileAsync(path);
-            NetDebug.Logger?.WriteNet(NetLogLevel.Info, $"Config is loaded from '{path}'.");
-
-            // Start server
-            var server = new Server.Server(config, Options.DefaultOptions);
-            await server.Start();
+                // Start server
+                var server = new Server.Server(config, Options.DefaultOptions);
+                await server.Start();
+            }
+            finally
+            {
+                logger.Flush();
+            }
         }
     }
 }
